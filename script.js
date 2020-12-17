cd5742bdb33bbd44e3d89ea316d3c537c82c3e73
Eric LaFontsee <elafontsee@gmail.com>
2020-12-17 14:04:11 -0600

    Basic calculator functions set up. Can perform addition, subtraction, multiplication, and division

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const calculator = document.querySelector(".calculator");
 const keys = document.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
+let firstNumber = "";
+let secondNumber = '';
+let operator = "";
+
 var operations = {
-  plus: function (a, b) {
+  add: function (a, b) {
     return a + b;
   },
-  minus: function (a, b) {
+  subtract: function (a, b) {
     return a - b;
   },
   multiply: function (a, b) {
@@ -16,21 +20,24 @@ var operations = {
   }
 };
 
-keys.addEventListener("click", (e) => {
+keys.addEventListener("click", e => {
   if (e.target.matches("button")) {
     const key = e.target;
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType
+
+    Array.from(key.parentNode.children).forEach(k => k.classList.remove("is-depressed"));
 
     if (!action) {
-      if(displayedNum === '0'){
+      if(displayedNum === '0' || previousKeyType === 'operator'){
           display.textContent = keyContent;
       }else{
           display.textContent += keyContent;
       }
-      console.log("number");
     }
+
     if(action === 'decimal'){
         display.textContent += '.'
     }
@@ -40,22 +47,46 @@ keys.addEventListener("click", (e) => {
       action === "multiply" ||
       action === "divide"
     ) {
-      // key.classList.add("is-depressed");
-      // calculator.dataset.previousKeyType = 'operator';
-      console.log("operator");
-    }
-
-    //   Array.from(key.parentNode.children).forEach(k => k.classList.remove("is-depressed"));
-    if (action === "decimal") {
-      console.log("decimal key!");
+      key.classList.add("is-depressed");
+      calculator.dataset.previousKeyType = 'operator';
+      firstNumber = displayedNum;
+      operator = action;
     }
 
     if (action === "clear") {
-      console.log("clear key!");
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
+        display.textContent = 0;
     }
 
     if (action === "calculate") {
-      console.log("equal key!");
+        secondNumber = displayedNum;
+
+        display.textContent = calculate(firstNumber, operator, secondNumber);
     }
   }
 });
+
+// function calculate(a, operator, b){
+//     var a = parseInt(firstNumber);
+//     var b = parseInt(secondNumber);
+//     var fn = operations[operator];
+//     result = fn(a,b);
+
+//     return result;
+// }
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }
+  
+    return result
+  }
